59a9ea820f78f2afadc10f140eb153d5f4401c51
Arum Lee <sarada032@gmail.com>
2019-06-04 19:01:58 -0400

    bg img, rotate

---
+++ bg.js
@@ -0,0 +1,24 @@
+const body = document.querySelector("body");
+
+const ImgNo = 10;
+
+function getImg(num){
+    const image = new Image();
+    image.src = `images/${num+1}.jpg`;
+    image.classList.add("bgimg");
+    body.prepend(image);
+  
+}
+
+function getRandom(){
+    //return Math.randomNumber();
+    return Math.floor(Math.random() * ImgNo);
+}
+
+function init(){
+const randomNumber = getRandom();
+getImg(randomNumber);
+} 
+
+init();
+
